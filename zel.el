8ba1331db38fdd21781918251d3c5a59cc62bfdb
Zach Codemaker <zachcodemaker@gmail.com>
2016-06-12 01:28:17 -0500

    Initial tests

--- zel.el
+++ zel.el
@@ -1,4 +1,13 @@
 ;;; -*- lexical-binding: t -*-
 
+(defmacro zel-test= (a b)
+  `(if (not (equal ,a ,b))
+       (error (format "\n(zel-test= %S %S)\nExpected %S, got %S" ',a ',b ,a ,b))
+     t))
+
 (defun zel-run-tests ()
+  (zel-test= t t)
+  (zel-test= t (not nil))
   t)
+
+(provide 'zel)
